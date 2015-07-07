f78a3760abb97e6318be15ef61ffd6b389c51a9b
glalica <gcabrera2@yahoo.com>
2015-07-08 00:23:57 +0100

    Primera pregunta

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log('app.js');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
+console.log("routes/index.js")
 var express = require('express');
 var router = express.Router();
-
+//Añado elcontrolador del quiz
+//Importo el enrutador
+var quizController = require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+//añado los get de pregunta y respuesta
+//Nos lleva a importar las acciones asociadas
+// a answer y a question
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
