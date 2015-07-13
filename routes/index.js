146abbb57c34cfda8f51cdf14d52ae68cb82ed01
glalica <gcabrera2@yahoo.com>
2015-07-13 05:00:36 +0100

    Ejercicio P2P

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,18 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+/*router.get('/', function(req, res) {
+  res.render('creditos', {title: 'Quiz'});
+}*/
+
+
 //añado los get de pregunta y respuesta
 //Nos lleva a importar las acciones asociadas
 // a answer y a question
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
+router.get('/author', quizController.author);
+
 
 module.exports = router;
