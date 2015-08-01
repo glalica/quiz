cd04dafaf7dbbb1c8fae9b353be0172f39843bdb
glalica <gcabrera2@yahoo.com>
2015-08-01 01:15:23 +0100

    BaseDatos

---
+++ models/models.js
@@ -0,0 +1,49 @@
+// Define como se construye todo el Modelo
+var path = require('path');
+
+// Postgres DATABASE_URL = prostgress://user:passwd@host:port/DATABASE_URL
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user    = (url[2]||null);
+var pwd     = (url[3]||null);
+var protocol= (url[1]||null);
+var dialect = (url[1]||null);
+var port    = (url[5]||null);
+var host    = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+// Cargar Modelo ORM (importa)
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite     ES creada la base particularizada
+var sequelize = new Sequelize(DB_name, user, pwd,
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // Solo SQLite (.env)
+    omitNull: true      // solo Postgres
+    }
+);
+
+// Importar la defición de la tabla Quiz en quiz.js
+//var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+// Se exporta pqra que pueda ser accedido desde otros lugares
+exports.Quiz =  Quiz;   // exportar definición de tabla quiz
+
+// sequelize.syn() crea e inicializa tabla de preguntas en DB
+sequelize.sync().success(function(){
+  // success(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().success(function (count){
+    if (count === 0) {  //la tabla se inicializa solo si está vacia
+      Quiz.create({ pregunta: 'Capital de Italia',
+                    respuesta: 'Roma'
+                  })
+    .success(function(){console.log('Base de datos inicializada')});
+  };
+});
+});
