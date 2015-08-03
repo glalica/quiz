08ab8e4e25ff384cb138b4f5c6e929df177724e7
glalica <gcabrera2@yahoo.com>
2015-08-03 13:17:07 +0100

    Lista de Preguntas1

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,21 +11,17 @@ exports.index = function(req, res){
 // GET /quizes/question
 //exports.question = function(req, res){
 exports.show = function(req,res){
-//models.Quiz.findAll().then(function(quiz) {
   models.Quiz.find(req.params.quizId).then(function(quiz){
-    res.render('quizes/show',{ quiz: quiz});
+    res.render('quizes/show', { quiz: quiz});
     //res.render('quizes/question',{pregunta: quiz[0].pregunta})
   })
 };
 
 //GET /quizes/answer
 exports.answer = function(req, res){
-  //console.log("req.query.respuesta" + req.query.respuesta);
-  //models.Quiz.findAll().then(function(quiz) {
-    models.Quiz.findById(req.params.quizId).then(function(quiz){
+    models.Quiz.find(req.params.quizId).then(function(quiz){
     if (req.query.respuesta === quiz.respuesta) {
-      //res.render('quizes/answer',{respuesta: 'Correcto'});
-      res.render('quizes/answer',
+          res.render('quizes/answer',
                 {quiz: quiz, respuesta: 'Correcto'});
     } else {
       res.render('quizes/answer',
