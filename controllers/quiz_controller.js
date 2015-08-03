e2abdbaa62f73ce2ee6e0f5d2148befbdeac74f3
glalica <gcabrera2@yahoo.com>
2015-08-03 20:37:33 +0100

    Borrar Pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -94,6 +94,12 @@ exports.update = function(req, res) {
   );
 };
 
+//Delete /quizes/:id
+exports.destroy = function(req, res){
+  req.quiz.destroy().then(function() {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
 
 exports.author = function(req, res){
   res.render('author', {});
