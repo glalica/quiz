bcc4325cbe73b30edfea0d28acbe8c370b7f593b
glalica <gcabrera2@yahoo.com>
2015-08-03 16:54:41 +0100

    Validación de Entradas

--- models/quiz.js
+++ models/quiz.js
@@ -3,8 +3,16 @@
 // Quiz es el constructor de objetos
 //pregunta y respuesta son las dos columnas con datos tipo string
  module.exports = function(sequelize, DataTypes) {
-   return sequelize.define('Quiz',
-          { pregunta: DataTypes.STRING,
-            respuesta: DataTypes.STRING,
-        });
- }
+   return sequelize.define(
+     'Quiz',
+    { pregunta:{
+      type: DataTypes.STRING,
+      validate: {notEmpty: {msg: "-> Falta Pregunta"}}
+      },
+      respuesta: {
+        type: DataTypes.STRING,
+        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+      }
+    }
+  );
+}
